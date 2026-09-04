cf4907968ae97f13e62ab7daeb5dfac58f607572
Gagan <gaganpratapsingh61@gmail.com>
2026-09-04 10:13:47 +0530

    initialise the project and serup env variables

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+
+  return (
+    <>
+     
+     <h1>hELLO WORLD! </h1>
+      
+    </>
+  )
+}
+
+export default App
